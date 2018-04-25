b88a1f7d1f3d1be87e0e89de6011e35f15d94019
mksvdmtr <38718095+mksvdmtr@users.noreply.github.com>
2018-04-25 11:49:28 +0300

    Add files via upload

---
+++ router_reboot.py
@@ -0,0 +1,22 @@
+import time
+from selenium import webdriver
+browser = webdriver.Firefox()
+browser.minimize_window()
+browser.get('http://sysadm:fReeD0mZ0I4@192.168.20.126')
+time.sleep(2)
+advance = browser.find_element_by_css_selector('#imgsrc')
+advance.click()
+time.sleep(2)
+system = browser.find_element_by_css_selector('#p_menu_misc > div:nth-child(2)')
+system.click()
+system_reboot = browser.find_element_by_css_selector('#c_menu_reboot > div:nth-child(2)')
+system_reboot.click()
+time.sleep(1)
+click_to_reboot = browser.find_element_by_css_selector('#reboot')
+time.sleep(2)
+click_to_reboot.click()
+time.sleep(1)
+alert = browser.switch_to.alert
+alert.accept()
+time.sleep(1)
+browser.quit()
